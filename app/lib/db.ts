4edeb0c76bc0dfba39254f5c06b458245edfa847
Gerald Metohu <geraldmetohu@gmail.com>
2025-09-14 20:50:12 +0100

    all

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,14 +1,5 @@
 // app/lib/db.ts
 import { PrismaClient } from "@prisma/client";
-
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    // log: ["query"], // uncomment if you want query logs
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
